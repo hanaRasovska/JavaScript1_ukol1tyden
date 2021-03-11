2373138ec566db7224ea4165af2b876528c02db1
Hana Rašovská <hana.rasovska@gmail.com>
2021-03-11 18:08:03 +0100

    ukol1odevzdani_pokus1

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 // tady je místo pro náš program
-
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
  * @param {int} a
  * @param {int} b
  */
-function secti(a, b) {
-  let c = a + b;
-  return c;
-}
+
 
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+  let text = document.querySelector("#ctverecek").innerHTML = "Gratulace!";
 }
+/** zmena barvy ctverecku**/
+function priKliknuti() {
+  let tlacCol;
+  tlacCol = document.querySelector(".ctverecek");
+  tlacCol.style.backgroundColor = "green";
+}
+/** zobrazeni vysledku**/
+function zobrazVysledek() {
+  let tlacRes;
+  tlacRes = document.querySelector("#vysledek").innerHTML = secti(10, 5);
+}
+
+function secti(a, b) {
+    let c = a + b;
+    return c;
+  }
+
+  
